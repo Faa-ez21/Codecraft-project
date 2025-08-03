3b828960cc835bf87a1333ede925f9ab33ec5aff
Erinntsua <erinntsua@gmail.com>
2025-08-03 17:13:39 +0000

    Add Facebook link to footer
    Updated the Facebook icon in the footer to link to the company's Facebook page instead of a placeholder.

--- apps/client-site/src/components/footer.jsx
+++ apps/client-site/src/components/footer.jsx
@@ -16,7 +16,7 @@ export default function Footer() {
         <div className="flex space-x-4 text-yellow-600 text-lg">
           <a href="https://x.com/expertfurnish?s=11" aria-label="Twitter"><FaTwitter /></a>
           <a href="https://www.instagram.com/expertofficefurnish?igsh=MW1kMHlyMXAyd24zMg==" aria-label="Instagram"><FaInstagram /></a>
-          <a href="#" aria-label="Facebook"><FaFacebookF /></a>
+          <a href="https://www.facebook.com/expertofficefurnishgh/" aria-label="Facebook"><FaFacebookF /></a>
           <a href="https://www.linkedin.com/company/expert-office-furnish-company-limited/" aria-label="LinkedIn"><FaLinkedinIn /></a>
         </div>
 
